603505b7a715bb0c49fa773bca2b5c15472cffc5
Vankka <vankka.main@gmail.com>
2022-09-05 18:05:54 +0300

    Add stuff to make properly distribute the components, bump version again

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { App } from "./app";
+export { Button } from "./button";
+export { CodeMirror } from "./codemirror";
+export { DiscordView } from "./discordview";
+export { Editor } from "./editor";
+export { Embed } from "./embed";
+export { DiscordInvite } from "./invite";
